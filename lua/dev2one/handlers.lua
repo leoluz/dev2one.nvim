6dc291b7e6a7c2ecaad6ac01c9516ab29ec83675
Leonardo Luz <leoluz@users.noreply.github.com>
2021-04-07 09:07:23 -0400

    implement the reference handler content

--- lua/dev2one/handlers.lua
+++ lua/dev2one/handlers.lua
@@ -1,19 +1,30 @@
 local window = require('dev2one.win')
 local content = require('dev2one.content')
+local M = {}
 
-local function document_symbol(err, _, result, _, bufnr)
+function M.document_symbol(err, _, result, _, bufnr)
   assert(not err, err)
   local winnr = vim.api.nvim_get_current_win()
   local c = content.from_document_symbol(result, bufnr)
-
-  local opt = {
+  local opts = {
     main_win = winnr,
     main_buf = bufnr
   }
-  local w = window.new(c, opt)
+  local w = window.new(c, opts)
   w.open()
 end
 
-return {
-  document_symbol=document_symbol
-}
+
+function M.document_references(err, _, result, _, bufnr)
+  assert(not err, err)
+  local winnr = vim.api.nvim_get_current_win()
+  local c = content.from_document_references(result)
+  local opts = {
+    main_win = winnr,
+    main_buf = bufnr
+  }
+  local w = window.new(c, opts)
+  w.open()
+end
+
+return M
