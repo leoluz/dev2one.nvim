4b000daf64433d38f3f8bd11d290b452e39c583f
Leonardo Luz <leoluz@users.noreply.github.com>
2021-04-13 19:51:24 -0400

    implement the first draft of gotest

--- lua/dev2one/handlers.lua
+++ lua/dev2one/handlers.lua
@@ -1,4 +1,4 @@
-local window = require('dev2one.win')
+local cmd = require('dev2one.cmd')
 local content = require('dev2one.content')
 local M = {}
 
@@ -8,22 +8,30 @@ local function handle(err, result, bufnr, content_fn)
   local c = content_fn(result, bufnr)
   local opts = {
     main_win = winnr,
-    main_buf = bufnr
+    main_buf = bufnr,
+    with_preview = true,
+    with_prompt = true
   }
-  local w = window.new(c, opts)
+  local w = cmd.window.new(c, opts)
   w.open()
 end
 
 function M.document_symbol(err, _, result, _, bufnr)
-  handle(err, result, bufnr, content.from_document_symbol)
+  if result ~= nil then
+    handle(err, result, bufnr, content.from_document_symbol)
+  end
 end
 
 function M.document_references(err, _, result, _, bufnr)
-  handle(err, result, bufnr, content.from_document_references)
+  if result ~= nil then
+    handle(err, result, bufnr, content.from_document_references)
+  end
 end
 
 function M.document_implementation(err, _, result, _, bufnr)
-  handle(err, result, bufnr, content.from_document_implementation)
+  if result ~= nil then
+    handle(err, result, bufnr, content.from_document_implementation)
+  end
 end
 
 return M
